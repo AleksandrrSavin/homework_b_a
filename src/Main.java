1594f8d51e5e779379e17fa5f053917e8b11f3b2
“Aleksandr <savin1984sasha@gmail.com>
2023-02-07 14:23:53 +0300

    homeworkadd

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 200;
+        int oneMileForTheAmount = 20;
+
+        int bonusMiles = price / oneMileForTheAmount;
+
+        System.out.println("Ваши бонусные мили "+bonusMiles);
+    }
+}
